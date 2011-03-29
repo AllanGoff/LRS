e0b3740aef533d7c810f1e9ba805b29acfbfc600
Allan Goff <AllanGoff@NovatiaLabs.com>
2011-03-29 09:35:58 -0700

    Initial commit - LRS.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsLRS::Application.config.secret_token = '13e687c1459146b5f58de5e4e6e3b28da5e19579e6b4350ebb33cc2460dda9a760c2324a555d31311a65da440329bca1dad82f7f9d07f2729305f47ed30468ee'
