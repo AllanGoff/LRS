3391cc5c69ed71bacde96fa3bd62f69eb16ad8a4
Allan Goff <AllanGoff@NovatiaLabs.com>
2011-03-30 10:38:06 -0700

    Add Pages controller.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def claims
+  end
+
+  def liens
+  end
+
+  def reports
+  end
+
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+#  describe "GET 'claims'" do       # Convert to integration test?
+#    it "should be successful" do
+#      get 'claims'
+#      response.should be_success
+#    end
+#  end
+
+  describe "GET 'liens'" do
+    it "should be successful" do
+      get 'liens'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'reports'" do
+    it "should be successful" do
+      get 'reports'
+      response.should be_success
+    end
+  end
+
+end
